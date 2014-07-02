f4501559318862630860941ed58c7bef06055c65
Tom Soulanille <soul@prama.com>
2014-07-02 15:17:51 -0700

    you can request a sequence of integers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,8 +2,17 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+angular.module('sprockApp.controllers', [])
+    .controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
+	      $scope.calcN = function() {
+		  $scope.N = Math.max(Math.min(20, parseInt($scope.text_N)))
+		  $http.post('/data', {N: $scope.N})
+		  .success(function (v) {
+			  console.log(v)
+			  $scope.results = v['count']
+			  console.log($scope.results)
+		      })
+	      }
 
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
