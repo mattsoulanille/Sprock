b728bb86a464019341130f70a02e95f84ca093e3
Tom Soulanille <soul@prama.com>
2014-07-02 22:41:37 -0700

    hack show server errors

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,17 +4,24 @@
 
 angular.module('sprockApp.controllers', [])
     .controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
-	      $scope.calcN = function() {
-		  $scope.N = Math.max(Math.min(20, parseInt($scope.text_N)))
-		  $http.post('/data', {N: $scope.N})
-		  .success(function (v) {
-			  console.log(v)
-			  $scope.results = v['count']
-			  console.log($scope.results)
-		      })
-	      }
+	$scope.serverError = ''
+	$scope.calcN = function() {
+	    $scope.N = Math.max(Math.min(20, parseInt($scope.text_N)))
+	    $http.post('/data', {N: $scope.N})
+		.success(function (v) {
+		    console.log(v)
+		    $scope.results = v['count']
+		    $scope.serverError = ''
+		    console.log($scope.results)
+		})
+	        .error(function(data, status, headers, config) {
+		    console.log(data)
+		    $scope.serverError = data
+		    $scope.results = []
+		})
+	}
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+    }])
+    .controller('MyCtrl2', ['$scope', function($scope) {
 
-  }]);
+    }]);
