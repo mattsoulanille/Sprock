9dc20242d7970973b3026eace4fe464cb2589985
Tom Soulanille <soul@prama.com>
2014-07-08 17:09:25 -0700

    (minor) tweak code format

--- app/js/app.js
+++ app/js/app.js
@@ -13,9 +13,12 @@ angular.module('sprockApp', [
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/view1', {
-       templateUrl: 'partials/partial1.html',
-       controller: 'MyCtrl1'
-     })
-    .when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'})
+	templateUrl: 'partials/partial1.html',
+	controller: 'MyCtrl1'
+    })
+    .when('/view2', {
+	templateUrl: 'partials/partial2.html',
+	controller: 'MyCtrl2'
+    })
     .otherwise({redirectTo: '/view1'})
 }]);
