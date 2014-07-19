302f1a5c37e4d380353a2ab2c64b29e1db6fecef
Tom Soulanille <soul@prama.com>
2014-07-18 19:48:07 -0700

    continuous greyscale for sequence quality

--- py/utils/make_css.py
+++ py/utils/make_css.py
@@ -1,13 +1,18 @@
+import pdb
 
 def main():
     s = r"""
 .qual%d {
   color: %s;
 }"""
+
+# [20..90] -> [lightest..0]
+
+    lightest = 234;
+    scale = float(lightest - 0) / (20 - 90)
     for q in xrange(21,91):
-        worth = ((q - 20)*15-1)/(90-20)
-        c = 'dcba98765432100'[worth]
-        color_string = '#%s%s%s' % (c, c, c)
+        c = int((q - 20) * scale + lightest)
+        color_string = '#%x%x%x' % (c, c, c)
         print(s % (q, color_string))
 
 if __name__ == '__main__':
