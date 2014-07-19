854fad76faad9b4229c948a759afc33241a515ed
Tom Soulanille <soul@prama.com>
2014-07-18 19:18:51 -0700

    differentiate sequence & integrate into HTML
    This is in preparation for merging in gene data, which will add events.

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,57 +3,31 @@
 /* Directives */
 
 
-angular.module('sprock.directives', ['underscore']).
+angular.module('sprock.directives', ['underscore', 'sprock.utilities']).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     }
   }]).
-  directive('formatSequence', ['_', function(_) {
-    function link(scope, elm, attrs) {
-      //console.log(elm)
+  directive('formatSequence',
+	    ['_',
+	     'differentiateSequenceToEvents',
+	     'integrateSequenceEventsToHTML',
+	     function(_, differentiateSequenceToEvents, integrateSequenceEventsToHTML ) {
+	       function link(scope, elm, attrs) {
+		 function updateSequenceDisplay(seqInfo) {
+		   elm.empty();
+		   seqInfo && elm.append(function(seqInfo) {
+		     return integrateSequenceEventsToHTML(differentiateSequenceToEvents(seqInfo));
+		   }(seqInfo));
+		 };
 
-      function updateSequenceDisplay(seq) {
-	elm.empty();
-	seq && elm.append(function(sequence, quals) {
+		 scope.$watch(attrs.formatSequence, function(seqInfo) {
+		   updateSequenceDisplay(seqInfo);
+		 });
+	       };
 
-	  function integrate(events) {
-	    // I wish native forEach() could be trusted to be in-order
-	    /* 
-	     * => '<span class="foo bar">ATCGC</span><span class="foo bar etc">...'
-	     */
-	    var s = '';
-	    for (var i = 0, length = events.length; i < length; i++) {
-	      var e = events[i];
-	    }
-	  }
-
-	  var l_sequence = sequence.length;
-	  var s = '<span class="seq qual' + quals[0] + '">';
-	  var prev_q = quals[0];
-	  s += sequence[0];
-	  var i = 1;
-
-	  while (i < l_sequence) {
-	    var q = quals[i];
-	    if (q != prev_q) {
-	      s += '</span><span class="seq qual' + q + '">';
-	      prev_q = q;
-	    }
-	    s += sequence[i];
-	    i += 1;
-	  }
-	  s += '</span>';
-	  return s;
-	}(seq['sequence'], seq['quality']));
-      };
-
-      scope.$watch(attrs.formatSequence, function(seq) {
-	updateSequenceDisplay(seq);
-      });
-    };
-
-    return {
-      link: link
-    };
-  }]);
+	       return {
+		 link: link
+	       };
+	     }]);
