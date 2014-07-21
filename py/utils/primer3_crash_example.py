87226e7ef3c40dfa14dc62f6c4a60b9ae0f2832e
Matthew Soulanille <matthew@Rime.local>
2014-07-21 11:13:18 -0700

    Changed the test program to make more sense.

--- py/utils/primer3_crash_example.py
+++ py/utils/primer3_crash_example.py
@@ -9,29 +9,26 @@ parser.add_argument('start', type=int, help='Coordinate to start at.', default=0
 parser.add_argument('end', type=int, help='Coordinate to end at.', default=2000)
 parser.add_argument('-q', action='store_true', help='include quality', default=False)
 parser.add_argument('-t', action='store_true', help='require primers to enclose the region 3000-3100', default=False)
-parser.add_argument('flag', type=str, nargs='?', help='add flags to the sequence arguments.')
+parser.add_argument('flag', type=str, nargs='*', help='add flags to the sequence arguments.')
 args=parser.parse_args()
 fastqfile = args.fastq
-
+print args.flag
 db = SeqIO.index(fastqfile, 'fastq', alphabet=IUPAC.ambiguous_dna)
 scaffold = 'Scaffold' + str(args.scaffold)
 record = db[scaffold][args.start:args.end]
 sequence = str(record.seq)
 qualList = record.letter_annotations["phred_quality"]
 qual = ' '.join([str(x) for x in qualList]) # It is unclear as to whether primer3 wants quality values in list format or in str format.
-seq_args = dict()
+seq_args = dict([x.split('=') for x in args.flag])
+print seq_args
 seq_args['SEQUENCE_TEMPLATE'] = sequence
-seq_args['SEQUENCE_QUALITY'] = qualList
-seq_only = dict()
-seq_only['SEQUENCE_TEMPLATE'] = sequence
+if args.q:
+    seq_args['SEQUENCE_QUALITY'] = qualList
 if args.t:
     seq_only['SEQUENCE_TARGET'] = [3000, 100]
     seq_args['SEQUENCE_TARGET'] = [3000, 100]
-if args.q:
-    print(seq_args)
-    print(primer3.designPrimers(seq_args, {}))
-else:
-    print(seq_only)
-    print(primer3.designPrimers(seq_only, {}))
+print(seq_args)
+print(primer3.designPrimers(seq_args, {}))
+
 
 
