87180f1728f6dbf7c8304bf1fc9f81cb9d8a82de
Tom Soulanille <soul@prama.com>
2014-07-22 11:10:28 -0700

    unittest for get_sequence

---
+++ py/utils/test_get_sequence.py
@@ -0,0 +1,24 @@
+import unittest
+
+from get_sequence import FQDB
+
+fastq_name = '/Users/soul/Projects/Bioinformatics/Echinobase/derived_data/Spur_3.1.LinearScaffold.fq'
+
+class GetSequenceTestCase(unittest.TestCase):
+    def setUp(self):
+        self.db = FQDB(fastq_name)
+
+    def tearDown(self):
+        self.db = None
+
+    def testGetSequence(self):
+        seq = self.db.get_sequence('Scaffold1', 1, 23)
+        assert seq == {'quality': [35, 35, 35, 32, 35, 53, 47, 41, 42, 46, 45, 29, 29, 29, 32, 33, 51, 51, 51, 51, 51, 51],
+                       'sequence': 'ACATTTTATCACCAGTTCGATT'}
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
