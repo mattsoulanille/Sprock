d7411b6cb1ea8c1bf3e5f988c0fabf6532b69416
Tom Soulanille <soul@prama.com>
2014-07-22 22:36:42 -0700

    testing getSequence but got lazy

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -5,10 +5,28 @@
 describe('service', function() {
   beforeEach(module('sprock.services'));
 
-
   describe('version', function() {
     it('should return current version', inject(function(version) {
       expect(version).toEqual('0.0.3');
     }));
   });
+
+  describe('getSequence', function() {
+    it('should be a function', inject(function(getSequence) {
+      expect(getSequence).toBeFunction();
+    }));
+    it('should return a promise', inject(function(getSequence) {
+      expect(getSequence()).toBeAPromise();
+    }));
+    xit('should produce good sequence data', inject(function(getSequence, $rootScope, $httpBackend) {
+      // FIXME
+      var sequence;
+      getSequence('Scaffold1', 2, 34).then(function(v) { sequence = v; });
+      expect(sequence).toBeUndefined();
+      $rootScope.$apply();
+      expect(sequence).toBe('getSequence happy');
+    }));
+
+  });
+
 });
