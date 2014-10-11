4de6070c25c7d6161739f8c3139d9748d74ac20e
Matthew <matthew@soulanille.net>
2014-10-11 14:24:14 -0700

    fixed quality css ranges to extend to 0 instead of 20

--- py/utils/make_css.py
+++ py/utils/make_css.py
@@ -6,12 +6,12 @@ def main():
   color: %s;
 }"""
 
-# [20..90] -> [lightest..0]
+# [0..90] -> [lightest..0]
 
     lightest = 234;
-    scale = float(lightest - 0) / (20 - 90)
-    for q in xrange(20,91):
-        c = int((q - 20) * scale + lightest)
+    scale = float(lightest - 0) / (0 - 90)
+    for q in xrange(0,91):
+        c = int((q - 0) * scale + lightest)
         color_string = '#%.2x%.2x%.2x' % (c, c, c)
         print(s % (q, color_string))
 
